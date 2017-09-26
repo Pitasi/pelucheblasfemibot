6c4cfedf829b0afa8f49669bde8ec943ee6a7eb2
Antonio Pitasi <pitasi.antonio@gmail.com>
2017-09-26 15:06:34 +0200

    updated stickers

--- replies.js
+++ replies.js
@@ -1,11 +1,26 @@
 module.exports = {
   // gifs
-  ebete: { type: 'gif', id: 'CgADBAADUQEAAmwOuFBmTOzUIz9ElAI' },
-  gabice: { type: 'gif', id: 'CgADBAADHQEAAisxKFG403Uvb_7mbwI' },
- 
-  // stickerzzz
-  bravo: { type: 'sticker', id: 'CAADBAADHxEAAmpj7wTIWvUa0fLgpQI' },
-  brao: { type: 'sticker', id: 'CAADBAADHxEAAmpj7wTIWvUa0fLgpQI' },
-  orcodio: { type: 'sticker', id: 'CAADBAAD6hAAAmpj7wSzUKVLXpHwfgI' },
+  "ebete": { type: 'gif', id: 'CgADBAADUQEAAmwOuFBmTOzUIz9ElAI' },
+  "gabice": { type: 'gif', id: 'CgADBAADHQEAAisxKFG403Uvb_7mbwI' },
 
+  // stickerzzz
+  "bravo": { type: 'sticker', id: 'CAADBAADHxEAAmpj7wTIWvUa0fLgpQI' },
+  "brao": { type: 'sticker', id: 'CAADBAADHxEAAmpj7wTIWvUa0fLgpQI' },
+  "orcodio": { type: 'sticker', id: 'CAADBAAD6hAAAmpj7wSzUKVLXpHwfgI' },
+  "fesso": { type: 'sticker', id: 'CAADBAAD_RAAAmpj7wSeHyWXUsBM2wI' },
+  "droghe": { type: 'sticker', id: 'CAADBAADChEAAmpj7wSnWSdk7jUybgI' },
+  "salve": { type: 'sticker', id: 'CAADBAADHBEAAmpj7wTH0sQg' },
+  "gne": { type: 'sticker', id: 'CAADBAADIREAAmpj7wS0vSdZB7ZkpgI' },
+  "vorresti": { type: 'sticker', id: 'CAADBAADKxEAAmpj7wRvkBqGbtvIEwI' },
+  "dannaz": { type: 'sticker', id: 'CAADBAAD-RAAAmpj7wQW8CEFONnCpwI' },
+  "fregato": { type: 'sticker', id: 'CAADBAADBBEAAmpj7wT0vQtY1snCjAI' },
+  "steso": { type: 'sticker', id: 'CAADBAADCREAAmpj7wQJOMnJ2sZPEgI' },
+  "defunto": { type: 'sticker', id: 'CAADBAADFREAAmpj7wRmyI0Msbp-JgI' },
+  "sparati": { type: 'sticker', id: 'CAADBAADLxEAAmpj7wTCRSc3MFDaXgI' },
+  "ma taci": { type: 'sticker', id: 'CAADBAADGREAAmpj7wTjy5l9yBpdqAI' },
+  "taci": { type: 'sticker', id: 'CAADBAADGREAAmpj7wTjy5l9yBpdqAI' },
+  "certo": { type: 'sticker', id: 'CAADBAADGxEAAmpj7wTWHIcy2F2PHwI' },
+  "furbo": { type: 'sticker', id: 'CAADBAADAREAAmpj7wS85LY5eKndaAI' },
+  "offeso": { type: 'sticker', id: 'CAADBAAD-xAAAmpj7wQ7BXm_t7iHJgI' },
+  "svea": { type: 'sticker', id: 'CAADBAAD_hAAAmpj7wS4xoO3Mcx-EQI' }
 }
