fc75ea21536640cad5835469a0ec45ed27788633
Antonio Pitasi <pitasi.antonio@gmail.com>
2017-09-26 11:43:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const Telegraf = require('telegraf');
+const token = 'API KEY';
+const bot = new Telegraf(process.env.TOKEN);
+const replies = require('./replies')
+ 
+// Restituisce l'id del messaggio di risposta se c'è,
+// altrimenti restituisce null
+function getReplyToMessageId(ctx) {
+  if (ctx.message.reply_to_message)
+    return ctx.message.reply_to_message.message_id
+  return null
+}
+ 
+// Funzione che preso il ctx e un elemento di replies manda la risposta
+// scegliendo il metodo giusto in base al tipo (gif o sticker)
+function sendReply(ctx, reply) {
+  let replyMethod = null
+ 
+  // replyMethod sarà la funzione con cui invieremo la risposta,
+  // cambia in base al tipo (gif / sticker)
+  if (reply.type === 'gif')
+    replyMethod = ctx.replyWithDocument
+  else if (reply.type === 'sticker')
+    replyMethod = ctx.replyWithSticker
+  else throw new Error('Tipo di risposta non valido.')
+ 
+  replyMethod(reply.id, {
+    reply_to_message_id: getReplyToMessageId(ctx)
+  })
+}
+ 
+bot.on('text', (ctx) => {
+  let cmd = ctx.message.text.toLowerCase()
+  if (cmd in replies)
+    sendReply(ctx, replies[cmd])
+});
+
+bot.startPolling();
---
+++ replies.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // gifs
+  ebete: { type: 'gif', id: 'CgADBAADUQEAAmwOuFBmTOzUIz9ElAI' },
+  gabice: { type: 'gif', id: 'CgADBAADHQEAAisxKFG403Uvb_7mbwI' },
+ 
+  // stickerzzz
+  bravo: { type: 'sticker', id: 'CAADBAADHxEAAmpj7wTIWvUa0fLgpQI' },
+  brao: { type: 'sticker', id: 'CAADBAADHxEAAmpj7wTIWvUa0fLgpQI' },
+  orcodio: { type: 'sticker', id: 'CAADBAAD6hAAAmpj7wSzUKVLXpHwfgI' },
+
+}
