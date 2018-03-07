9521d6b319d894dd28f99fae0bc5b4633ecdf22d
Antonio Pitasi <pitasi.antonio@gmail.com>
2018-03-07 22:06:06 +0100

    update triggers from bot

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 const Telegraf = require('telegraf');
+const fs = require('fs');
+
 const token = 'API KEY';
 const bot = new Telegraf(process.env.TOKEN);
-const replies = require('./replies')
+const admins = process.env.ADMINS ?
+  process.env.ADMINS.split(',').map(s => (parseInt(s.trim(), 10))) :
+  [];
+const replies = require('./replies');
  
 // Restituisce l'id del messaggio di risposta se c'è,
 // altrimenti restituisce null
 function getReplyToMessageId(ctx) {
   if (ctx.message.reply_to_message)
-    return ctx.message.reply_to_message.message_id
-  return null
+    return ctx.message.reply_to_message.message_id;
+  return null;
 }
  
 // Funzione che preso il ctx e un elemento di replies manda la risposta
 // scegliendo il metodo giusto in base al tipo (gif o sticker)
 function sendReply(ctx, reply) {
-  let replyMethod = null
+  let replyMethod = null;
  
   // replyMethod sarà la funzione con cui invieremo la risposta,
   // cambia in base al tipo (gif / sticker)
   if (reply.type === 'gif')
-    replyMethod = ctx.replyWithDocument
+    replyMethod = ctx.replyWithDocument;
   else if (reply.type === 'sticker')
-    replyMethod = ctx.replyWithSticker
-  else throw new Error('Tipo di risposta non valido.')
+    replyMethod = ctx.replyWithSticker;
+  else throw new Error('Tipo di risposta non valido.');
   
 
-  ctx.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id).catch(() => {})
+  ctx.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id).catch(() => {});
   replyMethod(reply.id, {
     reply_to_message_id: getReplyToMessageId(ctx)
-  })
+  });
 }
+
+bot.use((ctx, next) => {
+  ctx.state.isAdmin = admins.indexOf(ctx.from.id) >= 0;
+  ctx.state.isPrivate = ctx.chat.type === 'private';
+
+  next();
+})
 
 bot.command('lista', (ctx) => {
     ctx.reply(
@@ -39,6 +51,16 @@ bot.command('lista', (ctx) => {
 })
  
 bot.on('text', (ctx) => {
+  if (ctx.state.isPrivate && ctx.state.isAdmin && ctx.message.reply_to_message && ctx.message.reply_to_message.sticker) {
+    const id = ctx.message.reply_to_message.sticker.file_id;
+    const trigger = ctx.message.text;
+
+    replies[trigger] = { "type": "sticker", id };
+    fs.writeFileSync('./replies.json', JSON.stringify(replies), 'utf-8');
+
+    ctx.reply('Trigger saved :)');
+  }
+
   let cmd = ctx.message.text.toLowerCase()
   if (cmd in replies)
     sendReply(ctx, replies[cmd])
